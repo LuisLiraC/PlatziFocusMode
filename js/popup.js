5a6910344a15fe02db284abac4ad74c3a5f79545
LuisLiraC <luis_ssbm@hotmail.com>
2021-12-06 19:11:34 -0600

    [bugfix] MV UI after deactivate focus mode

--- js/popup.js
+++ js/popup.js
@@ -57,15 +57,24 @@ function removeFullScreen() {
   let MaterialView = document.querySelector(
     '.MaterialView.MaterialView-type--video'
   )
+  let MaterialViewVideoItem = document.querySelector('.MaterialView-video-item')
   let VideoPlayer = document.querySelector('.VideoPlayer > div')
   let Header = document.querySelector('.Header-v2.Header-v2-content')
   let Syllabus = document.querySelector('.Syllabus')
 
-  MaterialView.style.display = 'flex'
+  MaterialView.style.display = 'grid'
   MaterialView.style.paddingLeft = '32px'
 
-  VideoPlayer.style.zIndex = '999'
-  VideoPlayer.style.height = '759.375px'
+  VideoPlayer.style.zIndex = null
+
+  if (window.innerWidth >= 1440) {
+    VideoPlayer.style.height = '759.375px'
+  }
+
+  if (window.innerWidth < 1440) {
+    VideoPlayer.style.height = '0'
+    MaterialViewVideoItem.style.minHeight = 'auto'
+  }
 
   Header.style.display = 'grid'
   Syllabus.style.display = 'block'
